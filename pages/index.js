3db0c1424986bab44a918575223a6cd19d3ecedb
LaurentiuGhiga <ghiga_laurentiu@yahoo.com>
2022-03-28 00:52:43 +0200

    ultima caterinca

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,17 @@
 import Head from "next/head";
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <title>coi </title>
         <meta name="mucea" content="web" />
       </Head> 
-      <h1>Bine ai venit la panouri solare bre.</h1>
-      <p>Vrei sa cumperi?</p>
-      <p>Cate vrei?</p>
-      <p>Nu vrei, a?</p>
+      <h1 className={styles.title}>Bine ai venit la panouri solare bre.</h1>
+      <p className={styles.description}>Vrei sa cumperi?</p>
+      <p className={styles.description}>Cate vrei?</p>
+      <p className={styles.description}>Nu vrei, a?</p>
     </div>
   );
 }
