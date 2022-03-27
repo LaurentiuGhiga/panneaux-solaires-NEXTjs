785bc6bfdc0978d2fea5beb0f1b5730cceee02f5
LaurentiuGhiga <ghiga_laurentiu@yahoo.com>
2022-03-28 00:18:44 +0200

    test caterinca

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,9 @@ export default function Home() {
         <meta name="mucea" content="web" />
       </Head> 
       <h1>Bine ai venit la panouri solare bre.</h1>
+      <p>Vrei sa cumperi?</p>
+      <p>Cate vrei?</p>
+      <p>Nu vrei, a?</p>
     </div>
   );
 }
