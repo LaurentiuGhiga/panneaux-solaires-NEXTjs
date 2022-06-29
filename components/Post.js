eff1b1b5eabdd12de49cdb187f53e307b52a4733
LaurentiuGhiga <ghiga_laurentiu@yahoo.com>
2022-06-30 01:14:39 +0200

    modal post

--- components/Post.js
+++ components/Post.js
@@ -1,6 +1,27 @@
-import { Avatar } from "@mui/material";
+import { Avatar, IconButton } from "@mui/material";
+import MoreHorizRoundedIcon from "@mui/icons-material/MoreHorizRounded";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
+import ThumbUpOffAltOutlinedIcon from "@mui/icons-material/ThumbUpOffAltOutlined";
+import ThumbUpOffAltRoundedIcon from "@mui/icons-material/ThumbUpOffAltRounded";
+import { useRecoilState } from "recoil";
+import { handlePostState, getPostState } from "../atoms/postAtom";
+import { useState } from "react";
+import DeleteRoundedIcon from "@mui/icons-material/DeleteRounded";
+import ReplyRoundedIcon from "@mui/icons-material/ReplyRounded";
+import CommentOutlinedIcon from "@mui/icons-material/CommentOutlined";
+import { modalState, modalTypeState } from "../atoms/modalAtom";
+//import TimeAgo from "timeago-react";
+import { useSession } from "next-auth/react";
 
 function Post({ post, modalPost }) {
+  const [modalOpen, setModalOpen] = useRecoilState(modalState);
+  const [modalType, setModalType] = useRecoilState(modalTypeState);
+  const [postState, setPostState] = useRecoilState(getPostState);
+  const [showInput, setShowInput] = useState(false);
+
+  const truncate = (string, n) =>
+    string?.length > n ? string.substr(0, n - 1) + " ...see more" : string;
+
   return (
     <div
       className={`bg-white dark:bg-[#1D2226] ${
@@ -10,9 +31,45 @@ function Post({ post, modalPost }) {
       <div className="flex items-center px-2.5 cursor-pointer">
         <Avatar src={post.userImg} className="!h-10 !w-10 cursor-pointer" />
         <div className="mr-auto ml-2 leading-none">
-          <h6 className="font-medium hover:text-blue-500 hover:underline">{post.username}</h6>
+          <h6 className="font-medium hover:text-blue-500 hover:underline">
+            {post.username}
+          </h6>
+          <p className="text-sm dark:text-white/75 opacity-80">{post.email}</p>
+          {/* Timeago stamp */}
         </div>
+        {modalPost ? (
+          <IconButton onClick={() => setModalOpen(false)}>
+            <CloseRoundedIcon className="dark:text-white/75 h-7 w-7" />
+          </IconButton>
+        ) : (
+          <IconButton>
+            <MoreHorizRoundedIcon className="dark:text-white/75 h-7 w-7" />
+          </IconButton>
+        )}
       </div>
+      {post.input && (
+        <div className="px-2.5 break-before-all md:break-normal">
+          {modalPost || showInput ? (
+            <p onClick={() => setShowInput(false)}>{post.input}</p>
+          ) : (
+            <p onClick={() => setShowInput(true)}>
+              {truncate(post.input, 150)}
+            </p>
+          )}
+        </div>
+      )}
+      {post.photoUrl && !modalPost && (
+        <img
+          src={post.photoUrl}
+          alt=""
+          className="w-full cursor-pointer"
+          onClick={() => {
+            setModalOpen(true);
+            setModalType("gifYouUp");
+            setPostState(post);
+          }}
+        />
+      )}
     </div>
   );
 }
