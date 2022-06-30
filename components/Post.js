41a6b642a2039f7ad21a2906b331318fe8bb8189
LaurentiuGhiga <ghiga_laurentiu@yahoo.com>
2022-07-01 01:03:12 +0200

    button Like and Share

--- components/Post.js
+++ components/Post.js
@@ -14,13 +14,17 @@ import { modalState, modalTypeState } from "../atoms/modalAtom";
 import { useSession } from "next-auth/react";
 
 function Post({ post, modalPost }) {
+  const { data: session } = useSession();
   const [modalOpen, setModalOpen] = useRecoilState(modalState);
   const [modalType, setModalType] = useRecoilState(modalTypeState);
   const [postState, setPostState] = useRecoilState(getPostState);
   const [showInput, setShowInput] = useState(false);
+  const [liked, setLiked] = useState(false);
 
   const truncate = (string, n) =>
     string?.length > n ? string.substr(0, n - 1) + " ...see more" : string;
+
+  const deletePost = () => {};
 
   return (
     <div
@@ -70,6 +74,42 @@ function Post({ post, modalPost }) {
           }}
         />
       )}
+      <div className="flex justify-evenly items-center dark:border-t border-gray-600/80 mx-2.5 pt-2 text-black/60 dark:text-white/75">
+        {modalPost ? (
+          <button className="postButton">
+            <CommentOutlinedIcon />
+            <h4>Comment</h4>
+          </button>
+        ) : (
+          <button
+            className={`postButton ${liked && "text-blue-500"}`}
+            onClick={() => setLiked(!liked)}
+          >
+            {liked ? (
+              <ThumbUpOffAltRoundedIcon className="-scale-x-100" />
+            ) : (
+              <ThumbUpOffAltOutlinedIcon className="-scale-x-100" />
+            )}
+
+            <h4>Like</h4>
+          </button>
+        )}
+
+        {session?.user?.email === post.email ? (
+          <button
+            className="postButton focus:text-red-400"
+            onClick={deletePost}
+          >
+            <DeleteRoundedIcon />
+            <h4>Delete post</h4>
+          </button>
+        ) : (
+          <button className="postButton ">
+            <ReplyRoundedIcon className="-scale-x-100" />
+            <h4>Share</h4>
+          </button>
+        )}
+      </div>
     </div>
   );
 }
